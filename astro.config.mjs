059cda894a49880a110747099546f6642690cdc9
Nicmaa <nicoma.17fe@gmail.com>
2026-04-29 01:35:25 +0200

    Aggiunto servizi e aggiustato alcune cose

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,12 @@ import netlify from '@astrojs/netlify';
 import keystatic from '@keystatic/astro';
 import tailwindVite from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 export default defineConfig({
   output: 'static',
   adapter: netlify(),
-  integrations: [
-    react(),
-    keystatic()
-  ],
+  integrations: [react(), keystatic(), icon()],
   vite: {
     plugins: [tailwindVite()],
   }
